5de5195ded4b121bea9c5969b99464ca7142848c
Anggi Fitrahandika <anggifitra141@gmail.com>
2021-06-17 18:16:50 +0700

    Start
    Config Start Template

---
+++ application/models/M_login.php
@@ -0,0 +1,36 @@
+<?php
+class M_login extends CI_Model {
+/**
+ *  =======   Application By    : Anggi Fitrahandika  2019    =======
+ *  =======   Version           : V.1.0                       =======
+ *  =======   Contact           : anggifitra141@gmail.com     =======
+ *  ===========           Copyright 2019          ===================
+*/
+
+	public function check()
+	{
+		$this->db->where('username', $this->input->post('username', TRUE));
+		$this->db->where('password', md5($this->input->post('password', TRUE)));
+    $this->db->where('status', "ACTIVE", TRUE);
+		$query = $this->db->get('user');
+		if($query->num_rows() > 0)
+		{
+			$result = $query->row_array();
+			$data = array(
+					'id_user' => $result['id_user'],
+					'username' => $result['username'],
+          'fullname' => $result['fullname'],
+					'role' => $result['role'],
+          'status' => $result['status'],
+					'logged_in' => TRUE
+					);
+			$this->session->set_userdata($data);
+
+			return TRUE;
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
+}
